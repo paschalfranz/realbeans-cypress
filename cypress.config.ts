b0a240bf58191734e1316dec29b49485b17c435f
¨paschalfranz¨ <r0961282@student.thomasmore.be>
2025-05-27 02:07:26 +0200

    Limit test run to realbeans.cy.ts only

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,5 +7,11 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners if needed
     },
+    // Only include RealBeans spec
+    specPattern: "cypress/e2e/realbeans.cy.ts", // Only run this file
+    // If needed later, re-enable others:
+    // "cypress/e2e/home.cy.ts",
+    // "cypress/e2e/subscribe.cy.ts",
+    // "cypress/e2e/user-journey.cy.ts"
   },
 });
